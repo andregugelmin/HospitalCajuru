5192683a944d540396b64fc7507795d50fd09645
Andre-Heiden-Gugelmin <andre_gugelmin@hotmail.com>
2020-04-14 19:35:33 -0300

    Spaner do Terreno
    primeira versão do spwaner de terreno

---
+++ Hospital Cajuru/Assets/Ground.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField]
+    private float yspeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = new Vector3 (transform.position.x, transform.localPosition.y + yspeed * Time.deltaTime, transform.position.z);
+        if (transform.position.y > 6.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Hospital Cajuru/Assets/TerrainSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject terrainObj;
+    private Vector3 spawnPos;
+    [SerializeField]
+    private float spawnTime;
+    private float nextSpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSpawnTime = Time.time + spawnTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= nextSpawnTime)
+        {
+            SpawnTerrain();
+        }
+    }
+
+    private void SpawnTerrain()
+    {
+        spawnPos = new Vector3(Random.Range(-2.4f, 2.4f), -6.0f, transform.position.y);
+        Instantiate(terrainObj, transform.position + spawnPos, transform.rotation);
+        nextSpawnTime = Time.time + spawnTime;
+    }
+}
